15acaeccc2f93ecdef48cbcd7802be9671283db5
SarthakJShetty <sarthakshetty97@gmail.com>
2020-06-28 23:21:44 +0530

    Fixed the 10th -> 11th page issue as well using Selenium's exception handler.

--- ProposalScraper/Scraper.py
+++ ProposalScraper/Scraper.py
@@ -2,9 +2,13 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup as bs
 
 url = 'http://forestsclearance.nic.in/Wildnew_Online_Status_New.aspx'
+
+timeToSleep = 5
+pageToStartScrapping = 1
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -13,14 +17,20 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 browser = webdriver.Chrome(chrome_options=chrome_options)
 browser.get(url)
 browser.find_element_by_id('ctl00_ContentPlaceHolder1_Button1').click()
-time.sleep(5)
+time.sleep(timeToSleep)
 
-for counter in range(1, 138):
+if pageToStartScrapping != 1:
+	try:
+		browser.find_element_by_link_text(str(pageToStartScrapping)).click()
+		time.sleep(timeToSleep)
+	except NoSuchElementException:
+		browser.find_element_by_link_text("...").click()
+		time.sleep(timeToSleep)
+		browser.find_element_by_link_text(str(pageToStartScrapping)).click()
+		time.sleep(timeToSleep)
+
+for counter in range(pageToStartScrapping, 138):
 	proposalElements = []
-	'''What needs to be implented here?
-	1. Go through the page, scrape all the <span> elements.
-	2. Click on the next counter element to go to the next page.
-	3. Repeat'''
 	csvFile = open('proposal.csv', 'a')
 	htmlCode = browser.page_source
 	pageSoup = bs(htmlCode, 'html.parser')
@@ -34,6 +44,9 @@ for counter in range(1, 138):
 			csvFile.write('\n')
 		csvFile.write(proposalElement)
 		csvFile.write('\t')
-	csvFile.close()
-	browser.find_element_by_link_text(str(counter+1)).click()
-	time.sleep(5)
+	try:
+		browser.find_element_by_link_text(str(counter+1)).click()
+		time.sleep(timeToSleep)
+	except NoSuchElementException:
+		browser.find_element_by_link_text("...").click()
+		time.sleep(timeToSleep)
